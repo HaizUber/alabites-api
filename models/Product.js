3402e46953194c0c99e390dd845f1add5c407ef8
Meya <gabzmejia117@gmail.com>
2024-04-25 11:34:47 +0800

    removed some dependencies

--- models/Product.js
+++ models/Product.js
@@ -1,38 +1,33 @@
 const mongoose = require('mongoose');
+const Schmea = mongoose.Schema;
 
-const ProductSchema = new mongoose.Schema({
-    productName: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    availableStock: {
-        type: Number,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now()
+const ProductModel = new Schmea(
+    {
+        productName: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: false
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        availableStock: {
+            type: Number,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now()
+        }
     }
-});
+)
 
-let ProductModel;
-
-try {
-    ProductModel = mongoose.model('products');
-} catch (error) {
-    ProductModel = mongoose.model('products', ProductSchema);
-}
-
-module.exports = ProductModel;
+module.exports = mongoose.model('products', ProductModel);
