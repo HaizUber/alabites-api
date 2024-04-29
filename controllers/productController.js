7948806bc6d8e4ad4c8a5b1236b9e1fbfe726dac
Meya <gabzmejia117@gmail.com>
2024-04-29 11:44:10 +0800

    revert controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,79 +1,72 @@
 const Product = require('../models/Product');
 
-// Create a new product
 const createProduct = async (req, res) => {
-    const { name, description, price, store, category } = req.body;
+    const body = req.body;
+    console.log('userInfo ', req.userInfo);
     try {
-        const product = new Product({ name, description, price, store, category });
+        const product = new Product(body);
         const result = await product.save();
-        res.status(201).json({ message: "success", data: result });
+        res.status(201)
+            .json({ message: "created", result });
     } catch (err) {
-        console.error("Error creating product:", err);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500)
+            .json({ message: "Internal server error" });
     }
 }
 
-// Get all products
 const getProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
-        res.status(200).json({ message: "success", data: products });
+        const results = await Product.find({});
+        res.status(200)
+            .json({ message: "success", data: results });
     } catch (err) {
-        console.error("Error getting products:", err);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500)
+            .json({ message: "Internal server error" });
     }
 }
 
-// Get a product by ID
 const getProductById = async (req, res) => {
-    const { id } = req.params;
     try {
-        const product = await Product.findById(id);
-        if (!product) {
-            return res.status(404).json({ message: "Product not found" });
-        }
-        res.status(200).json({ message: "success", data: product });
+        const id = req.params.id;
+        const result = await Product.findById(id);
+        res.status(200)
+            .json({ message: "success", data: result });
     } catch (err) {
-        console.error("Error getting product by ID:", err);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500)
+            .json({ message: "Internal server error" });
     }
 }
 
-// Update a product by ID
 const updateProductById = async (req, res) => {
-    const { id } = req.params;
-    const { name, description, price, store, category } = req.body;
     try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, { name, description, price, store, category }, { new: true });
-        if (!updatedProduct) {
-            return res.status(404).json({ message: "Product not found" });
-        }
-        res.status(200).json({ message: "success", data: updatedProduct });
+        const id = req.params.id;
+        const body = req.body;
+        const updateDoc = { $set: { ...body } };
+        updateDoc.updatedAt = Date.now();
+        await Product.findByIdAndUpdate(id, updateDoc);
+        res.status(200)
+            .json({ message: "updated" });
     } catch (err) {
-        console.error("Error updating product by ID:", err);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500)
+            .json({ message: "Internal server error" });
     }
 }
 
-// Delete a product by ID
-const deleteProductById = async (req, res) => {
-    const { id } = req.params;
+const deleteById = async (req, res) => {
     try {
-        const deletedProduct = await Product.findByIdAndDelete(id);
-        if (!deletedProduct) {
-            return res.status(404).json({ message: "Product not found" });
-        }
-        res.status(200).json({ message: "success", data: deletedProduct });
+        const id = req.params.id;
+        await Product.findByIdAndDelete(id);
+        res.status(200)
+            .json({ message: "deleted" });
     } catch (err) {
-        console.error("Error deleting product by ID:", err);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500)
+            .json({ message: "Internal server error" });
     }
 }
-
 module.exports = {
     createProduct,
     getProducts,
     getProductById,
     updateProductById,
-    deleteProductById
+    deleteById
 }
