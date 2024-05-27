c8f4335149861da230d3a32b8ce139f8c0f1eda5
Meya <gabzmejia117@gmail.com>
2024-05-27 11:24:35 +0800

    feat: revision p1 of product schema

--- models/Product.js
+++ models/Product.js
@@ -21,21 +21,41 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    category: {
-        type: String,
-        required: true
-    },
-    productphoto: { // New field to store the photo reference
-        type: String,
+    productPhotos: { // Array to store multiple photo URLs
+        type: [String],
         required: false
+    },
+    stock: { // Field for stock management
+        type: Number,
+        required: true,
+        default: 0
+    },
+    tags: { // Field for product tags
+        type: [String],
+        required: false
+    },
+    discount: { // Field for discounts
+        type: Number,
+        required: false,
+        default: 0
+    },
+    averageRating: { // Field for average rating
+        type: Number,
+        required: false,
+        default: 0
+    },
+    numberOfReviews: { // Field for the number of reviews
+        type: Number,
+        required: false,
+        default: 0
     },
     createdAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     },
     updatedAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     }
 });
 
