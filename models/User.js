84c4a6854a5876b2e4a310a90455c924b722a089
Meya <gabzmejia117@gmail.com>
2024-06-12 09:17:49 +0800

    feat: removed firebaseuid requirement on user model

--- models/User.js
+++ models/User.js
@@ -26,10 +26,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    firebaseUID: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true
