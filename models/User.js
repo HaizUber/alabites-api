325a72c0f916db5478b2a17cf9608701ff9fb7b9
Meya <gabzmejia117@gmail.com>
2024-06-12 10:56:33 +0800

    feat: tamcredits payment method test

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
     type: {
         type: String,
-        enum: ['credit', 'debit'],
+        enum: ['tamcredits', 'debit'],
         required: true
     },
     amount: {
